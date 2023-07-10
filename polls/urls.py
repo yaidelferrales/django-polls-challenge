4d2be29d44529a0461cb3a8c83f0368dd4d7d60c
Alain Serrano <alains1984@gmail.com>
2023-07-10 16:57:59 +0200

    Updated urls poll

--- polls/urls.py
+++ polls/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 app_name = 'polls'
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
-    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
-    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
-    path('<int:question_id>/vote/', views.vote, name='vote'),
+    path('', views.QuestionViewList.as_view(), name='polls_list'),
+    path('<int:pk>/', views.QuestionDetailView.as_view(), name='polls_detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='polls_results'),
+    path('<int:question_id>/vote/', views.vote, name='polls_vote'),
 ]
