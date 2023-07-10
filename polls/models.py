ac622fc923be40503a96fc41248daa2e6de8c8e4
Alain Serrano <alains1984@gmail.com>
2023-07-10 16:41:01 +0200

    ordering pub-date in Question model

--- polls/models.py
+++ polls/models.py
@@ -5,16 +5,20 @@ from django.utils import timezone
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+    pub_date = models.DateTimeField(verbose_name='date publish')   
+    
     def __str__(self):
         return self.question_text
+    
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
-
+    class Meta:
+        ordering = ('-pub_date',)
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+    
     def __str__(self):
         return self.choice_text
