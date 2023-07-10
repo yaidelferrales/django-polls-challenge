49b3eaf0ba3d920ba7f2e37f12e1547db8e5dee0
Alain Serrano <alains1984@gmail.com>
2023-07-10 16:05:59 +0200

    Makemigrations and Migrate are done

--- project/settings.py
+++ project/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'NAME': 'postgres',
         'USER': 'postgres',
         'PASSWORD': 'postgres',
-        'HOST': '127.0.0.1',
+        'HOST': 'postgres',
         'PORT': '5432',
     }
 }
