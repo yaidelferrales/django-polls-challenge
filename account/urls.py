d3222340d89db3fbe8fc4b166853d80d5ec8eb0a
Alain Serrano <alains1984@gmail.com>
2023-07-10 16:19:16 +0200

    urls configurated

---
+++ account/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from . import views
+
+
+urlpatterns = [
+    path('', include('django.contrib.auth.urls')),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('register/', views.register, name='register'),
+    path('edit/', views.edit, name='edit')
+    
+]
